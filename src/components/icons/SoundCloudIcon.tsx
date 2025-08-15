ef8550fb76054db53101922f7f7927b570b68859
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-15 09:03:56 -0300

    components-icons content

---
+++ src/components/icons/SoundCloudIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SoundCloudIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M7 10.2v3.6h2.2c0-2-1-3.6-2.2-3.6zm-4.4 0v3.6h2.2c0-2-1-3.6-2.2-3.6zm13.1-4.1C12.3 6.1 9 7.7 9 10.2v5.6c0 1.8 1.5 3.1 3.3 3.1 1.8 0 3.3-1.4 3.3-3.1v-5.5c0-2-1.2-2.2-2.9-2.2zm-7.6.1c-1.8 0-3.3 1.4-3.3 3.1v2.3c0 1.8 1.5 3.1 3.3 3.1 1.8 0 3.3-1.4 3.3-3.1V9.4c0-1.7-1.5-3.1-3.3-3.1zM24 13.8v-3.6H21.8c.1 2-1 3.6-2.2 3.6z" />
+  </svg>
+);
+
+export default SoundCloudIcon;
